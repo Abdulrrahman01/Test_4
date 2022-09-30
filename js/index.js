c89e2cc8e2511523243ded0c0d795ebfaf96b5af
Abdulrrahman01 <112503426+Abdulrrahman01@users.noreply.github.com>
2022-09-30 18:12:12 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let checkbox = document.getElementById("checkbox");
+checkbox.onclick = function () {
+    checkbox.classList.toggle('on');
+}
+//----------------------------------------------------
+let checkbox1 = document.getElementById("checkbox1");
+checkbox1.onclick = function () {
+    checkbox1.classList.toggle('on');
+}
+// --------------------------------------------------------------
---
+++ js/index2.js
@@ -0,0 +1,16 @@
+let checkbox = document.getElementById("checkbox2");
+checkbox.onclick = function () {
+    checkbox.classList.toggle("on")
+}
+let checkbox2 = document.getElementById("checkbox3");
+checkbox2.onclick = function () {
+    checkbox2.classList.toggle("on")
+}
+let checkbox3 = document.getElementById("checkbox4");
+checkbox3.onclick = function () {
+    checkbox3.classList.toggle("on")
+}
+let checkbox4 = document.getElementById("checkbox5");
+checkbox4.onclick = function () {
+    checkbox4.classList.toggle("on")
+}
